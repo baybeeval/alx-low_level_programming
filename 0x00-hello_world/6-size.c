01d7ca9941651dd66d2f98e70d231132f2ce90eb
baybeeval <valentinemathai54@gmail.com>
2022-12-09 00:11:26 +0300

    Size of Variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+* main -Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int intType;
@@ -11,5 +15,5 @@ printf("Size of an int: %zu bytes\n");
 printf("Size of a long int: %lu bytes\n");
 printf("Size of a long long int: %llu bytes\n");
 printf("Size of a float: %zu bytes\n");
- return (0);
+return (0);
 }
