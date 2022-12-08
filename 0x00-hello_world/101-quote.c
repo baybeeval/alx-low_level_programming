f50945418b162a4d467301d8d4f292cefb3c0672
baybeeval <valentinemathai54@gmail.com>
2022-12-09 01:57:21 +0300

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
 * main - Entry Point
 *
 * Return: Always 1(Error)
 */
-
-
 int main(void)
 {
-write("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+char str4[] = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+write(str);
 return (1);
-
 }
