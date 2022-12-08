11ab3f77910d57af5fd3fb00114aa7bfc6902e7c
baybeeval <valentinemathai54@gmail.com>
2022-12-09 00:08:33 +0300

    Size of Variable

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+int intType;
+float floatType;
+double doubleTyple;
+char charType;
+printf("Size of a char: %zu bytes\n");
+printf("Size of an int: %zu bytes\n");
+printf("Size of a long int: %lu bytes\n");
+printf("Size of a long long int: %llu bytes\n");
+printf("Size of a float: %zu bytes\n");
+ return (0);
+}
