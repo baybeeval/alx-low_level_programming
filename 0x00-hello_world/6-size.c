0372a88f943ad23ee0b1a776419b8f4c00fafc52
baybeeval <valentinemathai54@gmail.com>
2022-12-09 00:50:30 +0300

    Size of Variable

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,11 @@ int intType;
 long int longintType;
 long long int longlongintType;
 float floatType;
-double doubleTyple;
 char charType;
-printf("Size of a char: %d bytes\n", sizeof(charType));
-printf("Size of an int: %d bytes\n", sizeof(intType));
-printf("Size of a long int: %d bytes\n", sizeof(longintType));
-printf("Size of a long long int: %d bytes\n", sizeof(longlongintType));
-printf("Size of a float: %d bytes\n", sizeof(floatType));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(intType));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(longintType));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(longlongintType));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floatType));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(charType));
 return (0);
 }
