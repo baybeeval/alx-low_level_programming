e0888efb66c5fa40120368adbce33aa704369c48
baybeeval <valentinemathai54@gmail.com>
2022-12-09 01:51:56 +0300

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,7 @@
 
 int main(void)
 {
-char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
-write(err);
+write("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
 return (1);
 
 }
