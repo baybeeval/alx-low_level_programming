bb6e30ee5ad4ede05d4d7d11bb6dcc7315be87f8
baybeeval <valentinemathai54@gmail.com>
2022-12-08 22:45:32 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 int main()
 /*using put to print the out put to return a value 0*/
 {
-    char string1[] ="Programming is like building a multilingual puzzle"
-    puts(string1);
+    
+    puts("Programming is like building a multilingual puzzle");
 
     return 0 ;
  }
