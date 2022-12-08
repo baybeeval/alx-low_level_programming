21a1fb4c5dd0e713c3e2023db6c7e4baae21846f
baybeeval <valentinemathai54@gmail.com>
2022-12-09 01:42:19 +0300

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - Entry Point
+*
+* Return: Always 1(Error)
+*/
+
+
+int main(void)
+{
+char err[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-199\n";
+return (1);
+
+}
