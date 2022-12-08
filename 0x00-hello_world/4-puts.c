557dfe5bc3033a0bf633df43770a371509b8f52f
baybeeval <valentinemathai54@gmail.com>
2022-12-08 22:50:29 +0300

    Puts C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#!/bin/bash
 #include<stdio.h>
 int main()
 /*using put to print the out put to return a value 0*/
 {
     
     puts("Programming is like building a multilingual puzzle");
+    getchar();
 
     return 0 ;
  }
