c0bfcac2faeeb42909416bcbf4eb599f0a9b75f5
baybeeval <valentinemathai54@gmail.com>
2022-12-10 04:08:49 +0300

    Print Tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Prints the alphabet in lowercase reverse
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char alp[26] = "zyxwvutsrqopnmlkjhgfedcba";
+int i;
+for (i = 0; i < 26; i--)
+{
+putchar(alp[i]);
+}
+putchar('\n');
+return (0);
+}
