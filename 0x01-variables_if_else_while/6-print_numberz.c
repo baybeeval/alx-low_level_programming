895c11219b6f42e4b7f7b7185feabc244c4029c3
baybeeval <valentinemathai54@gmail.com>
2022-12-10 03:15:03 +0300

    Print Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+*main - Print single digits of base 10
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int b =10;
+while (b<10)
+{
+putchar(b):
+b++;
+}
+return 0;
+}
