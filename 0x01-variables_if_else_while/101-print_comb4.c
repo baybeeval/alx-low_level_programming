c65c82fc990a04631b6b37e7f4860cfc2ac6c921
baybeeval <valentinemathai54@gmail.com>
2022-12-10 05:36:25 +0300

    Print comb 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-int num1, num2, nunt3;
+int num1, num2, num3;
 for (num1 = 0; num1 < 8; num1++)
 {
-for (num2 = nu1 + 1; digit2 < 9; digit2++)
+for (num2 = num1 + 1; num2 < 9; num2++)
 {
 for (num3 = num2 + 1; num3 < 10; num3++)
 {
