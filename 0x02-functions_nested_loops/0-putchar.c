ed1c09a65171de15b96f8dfe0441708d1835bae8
baybeeval <valentinemathai54@gmail.com>
2022-12-14 01:34:01 +0300

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "putchar.c"
+/**
+*main - Prints putchar
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char text[9] = "_putchar";
+int x = 0;
+for(x = 0; x < 8; x++)
+{
+_putchar(text[x]);
+}
+_putchar('\n');
+return (0);
+}
