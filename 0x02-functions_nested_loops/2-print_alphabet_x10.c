1303375cdb4b566b98796fd7c15939b8296a6154
baybeeval <valentinemathai54@gmail.com>
2022-12-14 03:03:50 +0300

    Alphabet times 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*print_alphabet - prints the alphabet in lowercase
+*
+*Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+int i
+char b;
+for (i = 0; i <= 10; i++)
+{
+for (b= 97; b <= 122; b++)
+{
+_putchar(b);
+}
+_puthcar('\n')
+}
