9ce557e45141404dc02b9f69331e0ed323dda40a
baybeeval <valentinemathai54@gmail.com>
2022-12-14 23:25:01 +0300

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*abs - checks code
+*
+*
+*Return: Always 0 (Success)
+*/
+int _abs(int)
+{
+int n;
+{
+if (n < 0)
+printf("The absolute value of n is %d\n",abs(-1));
+return (0);
+}
+else if (n > 0)
+{
+printf("The absolute value of n is %d\n",abs(1));
+return (0);
+}
+else
+{
+printf("The absolute value of n is %d\n",abs(0));
+}
+return (0);
+}
