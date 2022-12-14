ff14878fa18211c7dd6e155ddc9107a925fb7f3c
baybeeval <valentinemathai54@gmail.com>
2022-12-14 22:50:13 +0300

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*print_sign - print a sign of a number
+*print_sign - show if number is greater than,equal to or less than zero
 *
 *@n: n is an integer
 *
@@ -13,7 +13,7 @@ if (n > 0)
 _putchar(43);
 return (1);
 }
-else if(n < 0)
+else if (n < 0)
 {
 _putchar(45);
 return (-1);
