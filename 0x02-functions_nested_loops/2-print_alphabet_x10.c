fa69881938b1af19ac39f0eedfe46bfdabb31c02
baybeeval <valentinemathai54@gmail.com>
2022-12-14 03:11:49 +0300

    Alphabet times 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
-*print_alphabet - prints the alphabet in lowercase
+*print_alphabet_x10 - prints the alphabet in lowercase
 *
 *Return: Always 0 (Success)
 */
 void print_alphabet_x10(void)
 {
 int i, b;
-for (i = 0; i <= 10; i++)
+for (i = 0; i <= 9; i++)
 {
-for (b= 97; b <= 122; b++)
+for (b = 97; b <= 122; b++)
 {
 _putchar(b);
 }
