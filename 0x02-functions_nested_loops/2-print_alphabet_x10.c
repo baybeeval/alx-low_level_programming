8485fd60f2a4c6151f576e90f7d2e629de356923
baybeeval <valentinemathai54@gmail.com>
2022-12-14 03:09:56 +0300

    Alphabet times 10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,13 @@
 */
 void print_alphabet_x10(void)
 {
-int i
-char b;
+int i, b;
 for (i = 0; i <= 10; i++)
 {
 for (b= 97; b <= 122; b++)
 {
 _putchar(b);
 }
-_puthcar('\n')
+_putchar('\n');
+}
 }
