2a13daacc69f964183b75e5455b1a19d3c62ade9
baybeeval <valentinemathai54@gmail.com>
2022-12-16 02:43:01 +0300

    FIZZBUZZ

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,8 @@
-#include "main.h"
 #include <stdio.h>
 /**
 *main - prints Fizzbuzz, if multipes of 3 0r 5 or both
 *
-*Return: Always 0 (Success)
+*Return: number
 */
 int main(void)
 {
@@ -12,20 +11,25 @@ for (i = 1; i <= 100; i++)
 {
 if ((i % 3 == 0) && (i % 3 == 0)) 
 {
-printf("Fizz Buzz ");
-}
-else if (i % 5 == 0)
-{
-printf("Buzz ");
+printf("Fizz Buzz");
 }
 else if (i % 3 == 0) 
 {
-printf("Fizz ");
+printf("Fizz");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz");
+}
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
 }
+if (i != 100)
+{
+print("");
 }
+printf("\n");
 return 0;
 }
