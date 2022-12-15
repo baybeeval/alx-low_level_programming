c7b84e4ec81e2dc0cfb0abdfdbfe443742015db8
baybeeval <valentinemathai54@gmail.com>
2022-12-16 01:11:57 +0300

    FIZZBUZZ

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - Prints the Fizz Buzz test program
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++) 
+{
+if (i % 15 == 0) 
+{
+printf("Fizz Buzz ");
+} 
+else if (i % 3 == 0) 
+{
+printf("Fizz ");
+} 
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+} 
+else 
+{
+printf("%u ", i);
+}
+}
+return 0;
+}
