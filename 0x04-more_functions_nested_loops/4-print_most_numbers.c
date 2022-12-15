d96c4d598355e3beb17db04d751aa61880382364
baybeeval <valentinemathai54@gmail.com>
2022-12-15 21:10:46 +0300

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,12 +7,10 @@
 void print_most_numbers(void)
 {
 int x;
-for (x = 48 && x <= 57)
+for (x = 48; x <= 57; x++)
 {
-for (x != 50 && x != 52)
-{
+if (x != 50 && x != 52)
 _putchar(x);
-}
 }
 _putchar('\n');
 }
