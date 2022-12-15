56548c1bb1e527d2c517ee4e2651bc6b230f4985
baybeeval <valentinemathai54@gmail.com>
2022-12-15 03:39:41 +0300

    Is Alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
-*isalpha - checks the alphabet
+*_isalpha - checks the alphabet
+*@c: It is an integer
 *
 *Return: Always 1 else return 0
-*
 */
 int _isalpha(int c)
 {
-if ((c < 9 && c <= 122) || (c < 65 && c<= 90))
+if ((c < 9 && c <= 122) || (c < 65 && c <= 90))
 {
 return (1);
 }
