c30fa56552d22d97fb8b681ffb1574314ccea8c2
baybeeval <valentinemathai54@gmail.com>
2022-12-15 19:55:13 +0300

    C is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*isupper - print c if it is uppercase
+*
+*@c : Is an integer
+*
+* Return: 1 if c is uppercase, else return 0
+*/
+int _isupper(int c)
+{
+if(c >= 65 && c <= 132)
+{
+return (1);
+}
+else
+{
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+More coding in C programming.
