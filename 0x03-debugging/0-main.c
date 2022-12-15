288aedd32660162f56bf20ced8def3e5f8545c01
baybeeval <valentinemathai54@gmail.com>
2022-12-15 15:26:06 +0300

    Postitive or Negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*positive_or_negative - print number if positive or negative
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+i = 0;
+postivie_or_negative(i);
+return (0);
+}
