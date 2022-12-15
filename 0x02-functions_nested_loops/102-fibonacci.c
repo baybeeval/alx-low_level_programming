c693f5abc5cc9afdddc93617af8f01f47c4d790e
baybeeval <valentinemathai54@gmail.com>
2022-12-15 05:16:52 +0300

    Fibonacci Numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+*print - prints fibonacci numbers, start from 1 and 2
+*
+*Return: Always 0 (Success)
+*/
+void printFibonacciNumbers(int n)
+{
+int b1 = 1, b2 = 2, i;
+
+if (n < 1)
+return;
+printf("%d, ", b1);
+for (i = 1; i < n; i++) {
+printf("%d, ", b2);
+int next = b1 + b2;
+b1 = b2;
+b2 = next;
+}
+}
+int main()
+{
+printFibonacciNumbers(50);
+return (0);
+}
