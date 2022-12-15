7102f76da31c37eecfe098e90f7e7b47b7d82f62
baybeeval <valentinemathai54@gmail.com>
2022-12-15 20:08:19 +0300

    C is a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main."
+/**
+* _isdigit - check if c is a digit
+*@c : checks the number
+*
+* Return: 1 if c is a digit, else return 0
+*/
+int _isdigit(int c)
+{
+if (c = 0; c < 9; c++)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
