5efc4f027e80bf7803c0740c072067f3d9798310
baybeeval <valentinemathai54@gmail.com>
2022-12-16 02:36:39 +0300

    Print Number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -11,10 +11,10 @@ void print_number(int n)
 unsigned int x = n;
 if (n < 0)
 {
-_putchar('-');
+_putchar(45);
 x = -x;
 }
 if ((x / 10) > 0)
 print_number(x / 10);
-_putchar((x % 10) + '0');
+_putchar((x % 10) + 48);
 }
