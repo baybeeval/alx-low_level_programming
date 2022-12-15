96c182620a24caecd17b37db5e262263d4a94f61
baybeeval <valentinemathai54@gmail.com>
2022-12-16 00:33:40 +0300

    Print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,23 +1,23 @@
-#include "holberton.h"
+#include "main.h"
 /**
 * print_line - prints straight line n times.
 *
 *@n: times straight line is printed.
 *
-* Return: no return.
+* Return: no return
 */
 void print_line(int n)
 {
 int a;
 if (a <= 0)
 {
-  _putchar('\n')
+_putchar('\n');
 }
 else
 {
-for(a =0; a < n; a++)
+for (a = 0; a < n; a++)
 {
-_putchar('_');
+_putchar(95);
 }
 _putchar('\n');
 }
