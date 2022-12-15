b9cada4b609ab6b84d9f7d2153c76abd0dce1d82
baybeeval <valentinemathai54@gmail.com>
2022-12-15 05:52:56 +0300

    Fibonacci Numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 /**
-*print - prints fibonacci numbers, start from 1 and 2
+*main - prints fibonacci numbers, start from 1 and 2
 *
 *Return: Always 0 (Success)
 */
 int main(void)
 {
 int i;
-long int b1, b2, cd;  
+long int b1, b2, cd;
 b1 = 1;
 b2 = 2;
 printf("%ld, %ld", b1, b2);
 for (i = 0; i < 48; i++)
 {
 cd = b1 + b2;
-printf(",%ld", cd);
+printf(", %ld", cd);
 b1 = b2;
 b2 = cd;
 }
