2dae03e9e6f879e5a72af9044c58b310c466cefe
baybeeval <valentinemathai54@gmail.com>
2022-12-16 02:05:19 +0300

    FIZZBUZZ

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,16 +10,17 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++) 
 {
-if (i % 3 == 0) 
+if ((i % 3 == 0) && (i % 3 == 0)) 
 {
-printf("Fizz");
-} 
+printf("Fizz Buzz ");
+}
 else if (i % 5 == 0)
 {
-printf("Buzz");
-}  else if ((i % 3 == 0) && (i % 3 == 0)) 
+printf("Buzz ");
+}
+else if (i % 3 == 0) 
 {
-printf("Fizz Buzz");
+printf("Fizz ");
 }
 else
 {
