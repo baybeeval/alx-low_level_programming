134bd63614e3d96b86e692c8ffc632cb3149ee58
baybeeval <valentinemathai54@gmail.com>
2022-12-16 01:20:43 +0300

    FIZZBUZZ

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,22 +10,28 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 15 == 0)
+if ((i % 5 == 0) && (i % 3 == 0))
 {
-printf("Fizz Buzz ");
+printf("Fizz Buzz");
 }
 else if (i % 3 == 0)
 {
-printf("Fizz ");
+printf("Fizz");
 }
 else if (i % 5 == 0)
 {
-printf("Buzz ");
+printf("Buzz");
 }
 else
 {
-printf("%u ", i);
+printf("%d", i);
 }
+if (x != 100)
+{
+printf(" ");
+}
+}
+printf("\n");
 }
 return (0);
 }
