3fe8335e2108e40919faa13864a9614f4b44f123
baybeeval <valentinemathai54@gmail.com>
2022-12-16 00:18:03 +0300

    Print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* print_line - prints straight line n times.
+*
+*@n: times straight line is printed.
+*
+* Return: no return.
+*/
+void print_line(int n)
+{
+int a;
+if (a <= 0)
+{
+  _putchar('\n')
+}
+else
+{
+for(a =0; a < n; a++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
