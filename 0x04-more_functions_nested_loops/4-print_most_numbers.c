6a6bae5a2857469b21926752f432b56bdef38120
baybeeval <valentinemathai54@gmail.com>
2022-12-15 21:05:24 +0300

    Print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_most_numbers - print numbers from 0 to 9, withou 2 and 4
+*
+* Return: no return
+*/
+void print_most_numbers(void)
+{
+for (x = 48 && x <= 57)
+{
+for (x != 50 && x != 52)
+{
+_putchar(x);
+}
+}
+_putchar('\n');
+}
