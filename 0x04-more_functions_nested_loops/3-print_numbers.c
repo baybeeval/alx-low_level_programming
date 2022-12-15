b8179ed0e277ca82c24d4e80f936c3a4f483c508
baybeeval <valentinemathai54@gmail.com>
2022-12-15 20:44:19 +0300

    Print-numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,17 +2,14 @@
 /**
 * print_numbers - print number from 0 t0 9
 *
-*@x: is an integer
-*
 * Return: Always 0 (Success)
 */
 void print_numbers(void)
 {
 int x;
-for(x >= 0; x <=9 X++)
+for (x = 0; x <= 9; x++)
 {
 _putchar(x);
 }
 _putchar('\n');
-return (0);
 }
