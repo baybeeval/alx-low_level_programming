7ff510c6ef1fd6aa9809aef9c18cf25e0b639150
baybeeval <valentinemathai54@gmail.com>
2022-12-16 03:27:24 +0300

    Print Number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,26 +7,15 @@
 *Return: numbers 
 */
 void print_number(int n)
-{
-unsigned int t, w, c;
+unsigned int num = n;
 if (n < 0)
 {
-_putchar(45);
-t = n * -1;
+_putchar('-');
+num = -num;
 }
-else
+if ((num / 10) > 0)
 {
-t = n;
-}
-w = t;
-c = 1;
-while (w > 9)
-{
-w /= 10;
-c *= 10;
-}
-for (c = 0; c >= 1; c /= 10)
-{
-_putchar(((x / c) % 10) + 48);
+print_number(num / 10);
+_putchar((num % 10) + '0');
 }
 }
