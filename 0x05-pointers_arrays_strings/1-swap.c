8bbd87ff6482793a13c22d88593122a0a5bada93
baybeeval <valentinemathai54@gmail.com>
2022-12-19 21:13:44 +0300

    Swap to Values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*swap - swaps the values of two integers
+*
+*@a: is an integer
+*@b: is an integer
+*
+*Return: void
+*/
+void swap_int(int *a, int *b)
+{
+swap(&a, &b);
+}
