c0193f6bd8f9c8ed956c52439364185dcf760c79
baybeeval <valentinemathai54@gmail.com>
2022-12-19 23:39:25 +0300

    Rev_ string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,3 +1,12 @@
+#include "main.h"
+#include <studio.h>
+/**
+*rev_string - reverses a string
+*
+*@s: is a string
+*
+*Return: string
+*/
 void rev_string(char *s)
 {
   int start, end;
@@ -8,10 +17,9 @@ for (start = 0; start < end/2; start++)
 {
 c = s[start];
 s[start] = s[end - 1 - start];
-s[end - 1 - start] = c
+s[end - 1 - start] = c;
 }
 }
 for (start = 0; start < end; start++)
-_putchar(s[start]);
-_putchar('\n');
+printf(s[start]);
 }
