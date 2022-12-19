69911427724ec673508af3debd249e45f00284bf
baybeeval <valentinemathai54@gmail.com>
2022-12-19 22:31:05 +0300

    Puts string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,16 +2,19 @@
 /**
 *_puts - prints a string to stdout
 *
+*@str: is the string
+*
 *Returns: string
 */
 void _puts(char *str)
 {
 int c = 0;
-while (c >=0)
+while (c >= 0)
 {
-if (str[c] =='\0')
+if (str[c] == '\0')
 {
 _putchar('\n');
+break;
 }
 _putchar(str[c]);
 c++;
