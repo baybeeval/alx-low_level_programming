a74605cfaa330e8c0bc4b9a970da4ebec64e7897
baybeeval <valentinemathai54@gmail.com>
2022-12-20 02:52:05 +0300

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+/**
+*strcpy - print half of a string
+*
+*@dest: The destination
+*@src: The source
+*
+*Return: copy of string
+*/
+char *_strcpy(char *dest, char *src)
+{
+int x;
+for (x = 0; *(src + x) != '\0'; x++)
+{
+dest[x] = *(src + x);
+}
+dest[x] = '\0';
+return (dest);
+}
