0760693a884bca4e1ef49a97f9889d8ed543e7ef
baybeeval <valentinemathai54@gmail.com>
2022-12-19 23:35:33 +0300

    Rev_ string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+void rev_string(char *s)
+{
+  int start, end;
+int c = 0;
+ for (end = 0; s[end] != 0; end++)
+{
+for (start = 0; start < end/2; start++)
+{
+c = s[start];
+s[start] = s[end - 1 - start];
+s[end - 1 - start] = c
+}
+}
+for (start = 0; start < end; start++)
+_putchar(s[start]);
+_putchar('\n');
+}
