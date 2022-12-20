a9584077fe5ee8056b0f3d3b8febee89828dc100
baybeeval <valentinemathai54@gmail.com>
2022-12-20 21:45:08 +0300

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,7 +8,7 @@
 */
 int _atoi(char *s)
 {
-unsigned int count = 0, size = 0, x = 0, bd= 1, y = 1, i;
+unsigned int count = 0, size = 0, x = 0, bd = 1, y = 1, i;
 while (*(s + count) != '\0')
 {
 if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
@@ -28,5 +28,5 @@ for (i = count - size; i < count; i++)
 x = x + ((*(s + i) - 48) * y);
 y /= 10;
 }
-return (x * bd);
+return (x *bd);
 }
