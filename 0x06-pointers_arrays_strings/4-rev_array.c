4ae791d20afa40b288f3ae6afd0be387c6d4103f
baybeeval <valentinemathai54@gmail.com>
2022-12-23 00:42:59 +0300

    Reverse Array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,22 @@
+#include "main.h"
 /**
 *reverse_array- reverses the contents of an array
 *
-*@a: array 
+*@a: array
+*@n: numbers of the array
 *
 *Return: output reversed array
 */
 void reverse_array(int *a, int n)
 {
 int x, y, cont;
-for (x = 0; x < n-1; x++)
+for (x = 0; x < n - 1; x++)
 {
 for (y = x + 1; y > 0; y--)
 {
 cont = *(a + y);
 *(a + y) = *(a + (y - 1));
- *(a + (y - 1)) = cont;
+*(a + (y - 1)) = cont;
 }
 }
 }
