00b9c53a327ba7f629b64b9584d11e6b57a71243
baybeeval <valentinemathai54@gmail.com>
2022-12-22 23:15:05 +0300

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*_strcat - prints out concatenates two strings
+*_strcat - concatenates two strings
 *
 *@dest: destination
 *@src: source
@@ -16,9 +16,7 @@ while (dest[y] != '\0')
 y++;
 }
 for (x = 0 ; x < y && src[x] != '\0' ; x++)
-{
 dest[y + x] = src[x];
 dest[y + x] = '\0';
 return (dest);
-}
 }
