087235dc9b85faabe8035246d9c3a9b8393aeb53
baybeeval <valentinemathai54@gmail.com>
2022-12-23 02:53:13 +0300

    Captialize String

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,18 +10,18 @@ char *cap_string(char *s)
 {
 int x;
 int count = 0;
-int alpha[] = {32, 9,10,14,44,59,46,33,63,34,40,41,123,123};
+int alpha[] = {32, 9, 10, 14, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125} ;
 if (*(s + count) >= 97 && *(s + count) <= 122)
-*(s +count) = *(s + count) - 32;
-count ++;
+*(s + count) = *(s + count) - 32;
+count++;
 while (*(s + count) != '\0')
 {
 for (x = 0; x < 13; x++)
 {
-if (*(s + count) == alpha [x])
+if (*(s + count) == alpha[x])
 {
-if ((*(s + (count +1)) >= 97) && (*(s + (count + 1)) <= 122))
-*(s + (count +1)) = *(s + (count + 1)) - 32;
+if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count + 1)) = *(s + (count + 1)) - 32;
 break;
 }
 }
