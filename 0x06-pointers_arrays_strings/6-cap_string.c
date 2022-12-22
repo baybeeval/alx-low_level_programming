aec744e77c100708ec6db557149e9fdab65ba83c
baybeeval <valentinemathai54@gmail.com>
2022-12-23 02:46:44 +0300

    Captialize String

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+*cap_string - captializes all words of a string
+*
+*@s: string
+*
+*Return: uppercase string
+*/
+char *cap_string(char *s)
+{
+int x;
+int count = 0;
+int alpha[] = {32, 9,10,14,44,59,46,33,63,34,40,41,123,123};
+if (*(s + count) >= 97 && *(s + count) <= 122)
+*(s +count) = *(s + count) - 32;
+count ++;
+while (*(s + count) != '\0')
+{
+for (x = 0; x < 13; x++)
+{
+if (*(s + count) == alpha [x])
+{
+if ((*(s + (count +1)) >= 97) && (*(s + (count + 1)) <= 122))
+*(s + (count +1)) = *(s + (count + 1)) - 32;
+break;
+}
+}
+count++;
+}
+return (s);
+}
