8099b61b64c156f41cc5fec97170cd1feb54d216
baybeeval <valentinemathai54@gmail.com>
2022-12-23 03:28:30 +0300

    Leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+*leet - encodes string from characters to numbers
+*
+*@s: string
+*
+*Return: encoded string
+*/
+char *leet(char *s)
+{
+int x;
+if (s[x] == 'a' || s[x] == 'A')
+{
+s[x] = '4';
+}
+else if (s[x] == 'e' || s[x] == 'E')
+{
+s[x] = '3';
+}
+else if (s[x] == 'o' || s[x] == 'O')
+{
+s[x] = '0';
+}
+else if (s[x] == 't' || s[x] == 'T')
+{
+s[x] = '7';
+}
+else (s[x] == 'l' || s[x] == 'L')
+{
+s[x] = '1';
+}
+return (s);
+}
+
