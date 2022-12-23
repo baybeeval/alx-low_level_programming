7a63692ec50603ce89db7c278e30f4d68a14ea4a
baybeeval <valentinemathai54@gmail.com>
2022-12-23 05:12:31 +0300

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,13 +12,13 @@ int count = 0;
 int x;
 int lower[] = {97, 101, 111, 116, 108};
 int upper[] = {65, 69, 79, 84, 76};
-int num[] = {52, 51,48,55,49};
+int num[] = {52, 51, 48, 55, 49};
 while (*(s + count) != '\0')
 for (x = 0; x < 5; x++)
 {
 if (*(s + count) == lower[x] || *(s + count) == upper[x])
 {
-*(s +count) = num[x];
+*(s + count) = num[x];
 }
 count++;
 }
