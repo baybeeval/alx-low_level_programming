05ec2c5d88bb717b61f7ccd062bd8b9f619cb132
baybeeval <valentinemathai54@gmail.com>
2022-12-23 04:40:39 +0300

    Print Number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,13 +8,14 @@
 */
 void print_number(int n)
 {
-unsigned int x = n;
+unsigned int x;
 if (n < 0)
 {
 _putchar(45);
-x = -x;
+x *= -1;
 }
-if ((x / 10) > 0)
+x = n;
+if (x / 10)
 print_number(x / 10);
-putchar((x % 10) + '0');
+_putchar(x % 10 + '0');
 }
