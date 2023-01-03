f9ed1693da326f0d34a961f6b956574341258f09
baybeeval <valentinemathai54@gmail.com>
2023-01-03 23:19:45 +0300

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -15,12 +15,12 @@ for (a = 0; *(s + a); a++)
 for (b = 0; *(accept + b); b++)
 {
 if (*(s + a) == *(accept + b))
-{
 break;
 }
-if (*(accept + b)) != '\0')
 {
+if (*(accept + b) != '\0')
 return (s + a);
 }
 return (0);
+}
 }
