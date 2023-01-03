793737453fef167feb97f1dcba3015c71bd79294
baybeeval <valentinemathai54@gmail.com>
2023-01-04 02:20:00 +0300

    set string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*set_string -sets value of pointer to char
+*
+*@s: value of string one
+*@to: vlaue of string two
+*
+*Return: string
+*/
+void set_string(char **s, char *to)
+{
+*s = to;
+}
