acd1f1b0ed92602064953e6c1aabb9daf606138e
baybeeval <valentinemathai54@gmail.com>
2023-01-03 23:32:16 +0300

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,6 @@
 char *_strstr(char *haystack, char *needle)
 {
-char *s1 , *s2;
+char *s1, *s2;
 while (*haystack != '\0')
 {
 s1 = haystack;
@@ -10,9 +10,9 @@ while (*haystack != '\0' && *s2 != '\0' && *haystack == *s2)
 haystack++;
 s2++;
 }
-if(*s2 == '\0')
-return(s1);
+if (*s2 == '\0')
+return (s1);
 haystack = s1 + 1;
 }
-return(0);
+return (0);
 }
