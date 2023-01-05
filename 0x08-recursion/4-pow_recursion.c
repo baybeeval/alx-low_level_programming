7210f1c5473a8fd6ed11520dc8b2cb2c1c9ea69d
baybeeval <valentinemathai54@gmail.com>
2023-01-06 02:03:37 +0300

    pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
-*
 *_pow_recursion - returns value of x rasied to the power of y
 *
 *@x: number
 *@y: power number raised
 *
-*Return : Value of x
+*Return: Value of x raised by powery
 */
 int _pow_recursion(int x, int y)
 {
